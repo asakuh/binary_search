d6bbf0e941162ee3429d0a0c4c57cea4c1465684
朝倉ひかる <asakurahikaru@asakurahikarunoMacBook-Air.local>
2018-06-26 00:12:24 +0900

    spear決定

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int p ( int x ) {
+    int b = 0 , d , i;
+	b = 0;
+	for( i=0 ; i < n ; i++){
+	d = A[i] / x;
+	b = b+d;
+	}
+		 
+	if ( b < k ) return 0;
+	else return 1; 
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +22,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000001;
+  while ( ub - lb > 1 ) {
+    int m = ( lb + ub ) / 2;
+    if ( p(m) ) {
+        lb = m ;
+    }
+    else {
+        ub = m ;
+    }
+}
+printf ( "%d\n" , lb );
   return 0;
 }
